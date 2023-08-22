1b306240e1db38f1c56ef9e0df957f9307fbc05c
SlamTheDragon <98795769+SlamTheDragon@users.noreply.github.com>
2023-08-22 17:51:04 +0800

    added collapsable navigator

--- v3/src/components/widgets/ICommission/CPricesSection/CommissionPrices.tsx
+++ v3/src/components/widgets/ICommission/CPricesSection/CommissionPrices.tsx
@@ -1,54 +1,140 @@
+import { useState } from 'react'
 import { ReactComponent as Menu } from '@material-design-icons/svg/outlined/menu.svg'
 import { ReactComponent as Person } from '@material-design-icons/svg/outlined/person.svg'
 import { ReactComponent as Panorama } from '@material-design-icons/svg/outlined/panorama.svg'
 import { ReactComponent as Apps } from '@material-design-icons/svg/outlined/apps.svg'
-import style from './commissionPrices.module.scss';
+import { ReactComponent as Logo } from '../../../../assets/svg/SlamTheDragon Logo.svg'
+import style from './commissionPrices.module.scss'
+import Button from '../../../common/Button/Button'
+
+enum Target {
+	A,
+	B,
+	C,
+	D,
+}
 
 export default function CommissionPrices() {
+	const [getIsFolded, setIsFolded] = useState(false)
+	const [getTarget, setTarget] = useState<Target | null>(Target.A)
 
+	function toggleFold() {
+		setIsFolded(current => !current)
+	}
+
+	function renderView(target: Target | null) {
+		switch (target) {
+			case Target.A:
+				
+				return (
+					<>
+						a
+					</>
+				);
+			case Target.B:
+				
+				return (
+					<>
+						b
+					</>
+				);
+			case Target.C:
+				
+				return (
+					<>
+						c
+					</>
+				);
+			case Target.D:
+				
+				return (
+					<>
+						d
+					</>
+				);
+
+			default:
+				return (
+					<>
+						loading
+					</>
+				)
+		}
+	}
 
 	return (
 		<section className={style.PricesWrapper + " snap"} id='a'>
 			{/* background wrapper */}
 			<div className={style.pricesContainer}>
+				<div className={style.priceNavigator} style={{ width: `${getIsFolded ? 5.6 : 20}%` }}>
 
-				<div className={style.priceNavigator}>
-					<div>
+					<div className={style.s1}>
 						{/* hamburg */}
+						<Button onClick={toggleFold}>
+							<Menu />
+						</Button>
 
-						<Menu />
+						<Button onClick={
+							() => {
+								setTarget(Target.A)
+							}}
+							classItem={getTarget === Target.A ? style.btnSelected : ''}
+						>
+							<Logo />
+
+							{/* {getIsFolded ? null : <span>SlamTheDragon</span>} */}
+							<span>SlamTheDragon</span>
+						</Button>
 					</div>
-					<div>
+
+					<div className={style.s2}>
 						{/* profile */}
+						<Button onClick={
+							() => {
+								setTarget(Target.B)
+							}}
+							classItem={getTarget === Target.B ? style.btnSelected : ''}
+						>
+							<Person />
 
-						<Person />
+							{/* {getIsFolded ? null : <span>Portraits</span>} */}
+							<span>Portraits</span>
+						</Button>
 
-						<div>
-							Text
-						</div>
-					</div>
-					<div>
 						{/* illustrations */}
+						<Button onClick={
+							() => {
+								setTarget(Target.C)
+							}}
+							classItem={getTarget === Target.C ? style.btnSelected : ''}
+						>
+							<Panorama />
 
-						<Panorama />
+							{/* {getIsFolded ? null : <span>Illustrations</span>} */}
+							<span>Illustrations</span>
+						</Button>
 
-						<div>
-							Text
-						</div>
-					</div>
-					<div>
 						{/* others */}
+						<Button onClick={
+							() => {
+								setTarget(Target.D)
+							}}
+							classItem={getTarget === Target.D ? style.btnSelected : ''}
+						>
+							<Apps />
 
-						<Apps />
+							{/* {getIsFolded ? null : <span>Others</span>} */}
+							<span>Others</span>
+						</Button>
+					</div>
 
-						<div>
-							Text
-						</div>
+					<div className={style.s3}>
+						{/* {getIsFolded ? null : <span>Join my Discord!</span>} */}
 					</div>
 				</div>
 
-				<div className={style.priceViewer}>
-					logical rendering here
+				<div className={style.priceViewer} style={{ width: `${getIsFolded ? 94.4 : 80}%` }}>
+					{getTarget !== null && renderView(getTarget)}
 				</div>
 			</div>
 		</section>
